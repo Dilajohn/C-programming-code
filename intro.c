c2e37e31ffa3a4785f3d60ab6bbe0b6c6bcbd4b2
Dilajohn <okujaemmanuel97@gmail.com>
2025-09-25 12:21:30 +0000

    Refactor comments and code structure for clarity and consistency

--- intro.c
+++ intro.c
@@ -2,14 +2,26 @@
 
 //----c code structure---
 
-#include <stdio.h> //
+#include <stdio.h>    // is a preprocessor directive that includes the Standard Input Output library, 
 
 int main() {
 
 
-    printf("\tHello, World!\n");
-    printf("\tThis is my first c program.\n");
-    printf("\tLet's code together.....\n");
+    printf("\tHello, World!\n");       //statements print strings:
+    printf("\tThis is my first c program.\n");  //statements print strings:
+    printf("\tLet's code together.....\n");  //statements print strings:
 
-    return 0;
+    return 0;      //return 0; indicates that the program ended successfully.
 }
+
+//#include <stdio.h>: This is a preprocessor directive that includes the Standard Input Output library,
+//  which contains the definition for the printf function used for printing output.
+
+//main() Function: The entry point of every program.
+//  The code execution starts here.
+//  This function can declare local variables, call other functions,
+//  and control the flow of the program.
+
+//\t is a tab character for indentation.
+//\n is a newline character to move to the next line.
+
