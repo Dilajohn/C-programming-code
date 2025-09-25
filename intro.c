6bd2917163fa2c9be8fce0c605649c0764aec927
Dilajohn <okujaemmanuel97@gmail.com>
2025-09-25 12:09:40 +0000

    Implement code changes to enhance functionality and improve performance

---
+++ intro.c
@@ -0,0 +1,15 @@
+//-----introductory notes and code structure for the c program.
+
+//----c code structure---
+
+#include <stdio.h> //
+
+int main() {
+
+
+    printf("\tHello, World!\n");
+    printf("\tThis is my first c program.\n");
+    printf("\tLet's code together.....\n");
+
+    return 0;
+}
